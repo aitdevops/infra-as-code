23216b8e074a59cd485bc18e1b9c7c83f40c837b
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-08-30 02:02:27 -0400

    test

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+provider "google" {
+  project = devops-projects-426703
+}
+
+resource "google_service_account" "wif_service_account" {
+  account_id   = "wif-works-sa"
+  display_name = "WIF Works Service Account"
+  description  = "Service account for Workload Identity Federation"
+}
