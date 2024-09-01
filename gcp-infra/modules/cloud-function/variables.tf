0c6cae10334eafef481f2e9d36739005c1a7df0f
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-09-01 02:40:04 -0400

    infra

--- gcp-infra/modules/cloud-function/variables.tf
+++ gcp-infra/modules/cloud-function/variables.tf
@@ -1,20 +1,20 @@
-# variable "project_id" {
-#   description = "The GCP project ID"
-#   type        = string
-# }
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
 
-# variable "region" {
-#   description = "The GCP region"
-#   type        = string
-#   default     = "us-central1"
-# }
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "us-central1"
+}
 
-# variable "service_account_email" {
-#   description = "The email of the service account to use for the Cloud Run service"
-#   type        = string
-# }
+variable "service_account_email" {
+  description = "The email of the service account to use for the Cloud Run service"
+  type        = string
+}
 
-# variable "zip_file" {
-#   description = "The path to the zip file containing the function code"
-#   type        = string
-# }
+variable "zip_file" {
+  description = "The path to the zip file containing the function code"
+  type        = string
+}
