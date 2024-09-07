1c911ab2291d090e8b9b1f4c14b7e2050c1f5932
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-09-07 18:55:13 -0400

    azure-vault

--- azure-infra/environments/prod/backend.tf
+++ azure-infra/environments/prod/backend.tf
@@ -4,9 +4,6 @@ terraform {
     storage_account_name  = "terraformstateaitdevops"
     container_name        = "aitdevops"
     key                   = "terraform.tfstate"
-    subscription_id       = var.subscription_id  # Pull from environment variable or explicit variable
-    tenant_id             = var.tenant_id        # Pull from environment variable or explicit variable
-    client_id             = var.client_id        # Pull from environment variable or explicit variable
     use_oidc              = true
   }
 }
