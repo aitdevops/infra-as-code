7899a745f89dc25cf63fae29cc5df3cf5efee364
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-09-07 19:04:52 -0400

    azure-vault

--- azure-infra/environments/prod/provider.tf
+++ azure-infra/environments/prod/provider.tf
@@ -1,29 +1,7 @@
 provider "azurerm" {
   features {}
   use_oidc        = true
-  subscription_id = data.azurerm_key_vault_secret.azure_subscription_id.value
-  tenant_id       = data.azurerm_key_vault_secret.azure_tenant_id.value
-  client_id       = data.azurerm_key_vault_secret.azure_client_id.value
-}
-
-# Reference the existing Key Vault 'aitdevops-vault'
-data "azurerm_key_vault" "existing" {
-  name                = "aitdevops-vault"
-  resource_group_name = "aitdevops"  # Adjust this to the correct resource group if needed
-}
-
-# Fetch secrets from the existing Key Vault
-data "azurerm_key_vault_secret" "azure_subscription_id" {
-  name         = "azure-subscription-id"
-  key_vault_id = data.azurerm_key_vault.existing.id
-}
-
-data "azurerm_key_vault_secret" "azure_tenant_id" {
-  name         = "azure-tenant-id"
-  key_vault_id = data.azurerm_key_vault.existing.id
-}
-
-data "azurerm_key_vault_secret" "azure_client_id" {
-  name         = "azure-client-id"
-  key_vault_id = data.azurerm_key_vault.existing.id
+  subscription_id = var.azure_subscription_id  # Use GitHub Secret passed via environment variable
+  tenant_id       = var.azure_tenant_id        # Use GitHub Secret passed via environment variable
+  client_id       = var.azure_client_id        # Use GitHub Secret passed via environment variable
 }
