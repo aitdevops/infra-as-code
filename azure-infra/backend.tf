9a3685741df6020d40a6eae83795f4d0cbfb58c0
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-09-07 01:12:45 -0400

    azure-wif

--- azure-infra/backend.tf
+++ azure-infra/backend.tf
@@ -5,9 +5,10 @@ terraform {
     container_name        = "terraformstateaitdevops"
     key                   = "terraform.tfstate"
     
-    # Required for OIDC authentication
+    # Add Subscription ID, Tenant ID, and Client ID for OIDC authentication
+    subscription_id       = "6e5394a9-f79c-4c60-9252-45d6d4a0f88c"  # Your subscription ID
     tenant_id             = "4c470cc0-914e-41d8-b3f2-2812cb01d186"  # Your tenant ID
     client_id             = "d385aa71-0f89-4204-964c-2cbb4322fcc0"  # Your client ID
-    use_oidc              = true  # Enable OIDC
+    use_oidc              = true  # Enable OIDC authentication
   }
 }
--- azure-infra/main.tf
+++ azure-infra/main.tf
@@ -1,9 +1,11 @@
 provider "azurerm" {
   features {}
-  tenant_id  = "4c470cc0-914e-41d8-b3f2-2812cb01d186"  # Tenant ID
-  client_id  = "d385aa71-0f89-4204-964c-2cbb4322fcc0"  # Client ID
-  use_oidc   = true
+  subscription_id = "6e5394a9-f79c-4c60-9252-45d6d4a0f88c"
+  tenant_id       = "4c470cc0-914e-41d8-b3f2-2812cb01d186"
+  client_id       = "d385aa71-0f89-4204-964c-2cbb4322fcc0"
+  use_oidc        = true
 }
+
 
 
 
