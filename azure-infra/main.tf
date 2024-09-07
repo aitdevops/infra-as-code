d51078eba374f58d105fb665547f1d96fb36efc3
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-09-07 01:01:10 -0400

    azure-wif

--- azure-infra/main.tf
+++ azure-infra/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "aitdevops"
-    storage_account_name  = "terraformstateaitdevops"
-    container_name        = "terraformstateaitdevops"
-    key                   = "terraform.tfstate"
-  }
-}
-
 provider "azurerm" {
   features {}
   use_oidc = true
@@ -24,13 +15,4 @@ resource "azurerm_virtual_network" "example_vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-}
-
-# Output for resource group and virtual network
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
-
-output "vnet_id" {
-  value = azurerm_virtual_network.example_vnet.id
 }
