9121b81c09f0d92d4e31df471be426acf8462231
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-09-07 18:51:28 -0400

    azure-vault

--- azure-infra/environments/prod/backend.tf
+++ azure-infra/environments/prod/backend.tf
@@ -1,13 +1,12 @@
-# Backend configuration for storing Terraform state in Azure Blob Storage
 terraform {
   backend "azurerm" {
     resource_group_name   = "aitdevops"
     storage_account_name  = "terraformstateaitdevops"
     container_name        = "aitdevops"
     key                   = "terraform.tfstate"
-    subscription_id       = data.azurerm_key_vault_secret.azure_subscription_id.value
-    tenant_id             = data.azurerm_key_vault_secret.azure_tenant_id.value
-    client_id             = data.azurerm_key_vault_secret.azure_client_id.value
+    subscription_id       = var.subscription_id  # Pull from environment variable or explicit variable
+    tenant_id             = var.tenant_id        # Pull from environment variable or explicit variable
+    client_id             = var.client_id        # Pull from environment variable or explicit variable
     use_oidc              = true
   }
 }
--- azure-infra/environments/prod/variables.tf
+++ azure-infra/environments/prod/variables.tf
@@ -8,3 +8,17 @@ variable "location" {
   description = "Azure region where the resource group will be created."
   type        = string
 }
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure tenant ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure client ID"
+}
