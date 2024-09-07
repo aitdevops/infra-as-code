5fa5944b183da7d13281a63c1fdc3b81c8fe1f4a
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-09-07 00:10:37 -0400

    azure-wif

--- azure-infra/environments/prod/backend.tf
+++ azure-infra/environments/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "aitdevops"                    # Name of the Resource Group
-    storage_account_name  = "terraformstateaitdevops"      # Storage Account for the state
-    container_name        = "terraformstateaitdevops"      # Container for the state
-    key                   = "terraform.tfstate"            # Name of the state file
-    subscription_id       = var.subscription_id            # Optional: Ensure the right subscription is used
+    resource_group_name   = "aitdevops"
+    storage_account_name  = "terraformstateaitdevops"
+    container_name        = "terraformstateaitdevops"
+    key                   = "terraform.tfstate"
+    # No subscription_id here, will be provided via environment variable
   }
 }
