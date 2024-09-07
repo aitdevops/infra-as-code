67c32f68b051ab10c33131a22432ebb5e64efcac
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-09-06 23:39:04 -0400

    azure-wif

---
+++ azure-infra/modules/blob-storage/main.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {
+  features = {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-backend"
+  location = "East US"
+}
+
+# Storage Account
+resource "azurerm_storage_account" "sa" {
+  name                     = "terraformbackendstore" # Must be globally unique
+  resource_group_name       = azurerm_resource_group.rg.name
+  location                  = azurerm_resource_group.rg.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+}
+
+# Storage Container for Terraform State Files
+resource "azurerm_storage_container" "terraform_state" {
+  name                  = "aitdevops-tfstate"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
+
+# Storage Account Access Key (for backend configuration)
+output "storage_account_access_key" {
+  value = azurerm_storage_account.sa.primary_access_key
+  sensitive = true
+}
