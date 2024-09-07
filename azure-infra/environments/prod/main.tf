995f7c402ed6bed8cb0f3a340595b0df5ad25828
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-09-07 16:37:35 -0400

    encryption

--- azure-infra/environments/prod/main.tf
+++ azure-infra/environments/prod/main.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
   source              = "../../modules/resource_group"
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.rg_group_name
   location            = var.location
 }
