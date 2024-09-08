1b52ddf65193bec41e045461ffdace8e65a60b5e
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-09-07 22:18:55 -0400

    vnet

--- azure-infra/environments/prod/main.tf
+++ azure-infra/environments/prod/main.tf
@@ -4,15 +4,17 @@ module "resource_group" {
   location            = var.location
 }
 module "network" {
-  source              = "../../modules/network"
-  vnet_name           = "my-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = "East US"
-  resource_group_name = "aitdevops"
-  tags                = { "Environment" = "Production" }
-  private_subnet_name = "my-private-subnet"
+  source                    = "../../modules/network"
+  vnet_name                 = "my-vnet"
+  address_space             = ["10.0.0.0/16"]
+  location                  = var.location
+  resource_group_name       = var.resource_group_name
+  tags                      = { "Environment" = "Production" }
+  private_subnet_name       = "my-private-subnet"
   private_subnet_address_prefixes = ["10.0.1.0/24"]
-  nat_gateway_name    = "my-nat-gateway"
+  nat_gateway_name          = "my-nat-gateway"
   public_ip_address_id      = azurerm_public_ip.nat_gateway_ip.id
 }
+
+
 
--- azure-infra/modules/network/main.tf
+++ azure-infra/modules/network/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_nat_gateway" "nat_gateway" {
 }
 
 resource "azurerm_public_ip" "nat_gateway_ip" {
-  name                = "${var.nat_gateway_name}-ip"
+  name                = "my-nat-gateway-ip"
   location            = var.location
   resource_group_name = var.resource_group_name
   allocation_method   = "Static"
