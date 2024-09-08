48494647bbc9d5f38c8bf9c8bf7875fabdd7b619
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-09-07 22:49:37 -0400

    vnet

--- azure-infra/environments/prod/provider.tf
+++ azure-infra/environments/prod/provider.tf
@@ -2,6 +2,6 @@ provider "azurerm" {
   features {}
   use_oidc        = true
   subscription_id = var.azure_subscription_id  # Use GitHub Secret passed via environment variable
-  tenant_id       = var.azure_tenant_id        # Use GitHub Secret passed via environment variable
+  tenant_id       = var.azure_tenant_id        # UseGitHub Secret passed via environment variable
   client_id       = var.azure_client_id        # Use GitHub Secret passed via environment variable
 }
