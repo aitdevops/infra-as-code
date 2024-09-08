7358ccfc77be6d8c48081632ac53172a5a05b689
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-09-08 15:51:00 -0400

    aks

--- azure-infra/environments/prod/azure.tfvars
+++ azure-infra/environments/prod/azure.tfvars
@@ -1,6 +1,5 @@
 # vnet variables.
 prefix = "aitdevops"
-resource_group_name = "aitdevops"
 address_space = ["10.0.0.0/16"]
 address_prefixes = ["10.0.0.0/24"]
 location           = "East US"
--- azure-infra/modules/aks/main.tf
+++ azure-infra/modules/aks/main.tf
@@ -3,20 +3,26 @@
 # Reference existing VNet and Subnet
 data "azurerm_virtual_network" "vnet" {
   name                = "aitdevops-vnet"
-  resource_group_name = var.resource_group_name
+  resource_group_name = "${var.prefix}-rg"
 }
 
 data "azurerm_subnet" "subnet" {
   name                 = "aitdevops-subnet"
   virtual_network_name = data.azurerm_virtual_network.vnet.name
-  resource_group_name = var.resource_group_name
+  resource_group_name = "${var.prefix}-rg"
+}
+
+data "azurerm_" "subnet" {
+  name                 = "aitdevops-subnet"
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  resource_group_name = "${var.prefix}-rg"
 }
 
 # AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "${var.prefix}-aks"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = "${var.prefix}-rg"
   dns_prefix          = "${var.prefix}-aks"
 
   default_node_pool {
