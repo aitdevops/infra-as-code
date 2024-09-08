e92e2d48903fd955bd065ee36feace08644ef072
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-09-08 16:04:56 -0400

    aks

--- azure-infra/modules/aks/main.tf
+++ azure-infra/modules/aks/main.tf
@@ -1,20 +1,21 @@
-# Reference existing VNet and Subnet
+# Reference existing VNet
 data "azurerm_virtual_network" "vnet" {
   name                = "aitdevops-vnet"
-  resource_group_name = "${var.prefix}-rg"
+  resource_group_name = "aitdevops-rg" # Adjust if necessary
 }
 
+# Reference existing Subnet
 data "azurerm_subnet" "subnet" {
   name                 = "aitdevops-subnet"
   virtual_network_name = data.azurerm_virtual_network.vnet.name
-  resource_group_name  = "${var.prefix}-rg"
+  resource_group_name  = "aitdevops-rg" # Adjust if necessary
 }
 
 # AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "${var.prefix}-aks"
   location            = var.location
-  resource_group_name = "${var.prefix}-rg"
+  resource_group_name = "aitdevops-rg" # Use existing resource group
   dns_prefix          = "${var.prefix}-aks"
 
   default_node_pool {
