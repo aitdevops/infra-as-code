98a6f765152d697f6301b358c9afe1e574e16b7a
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-09-07 22:03:53 -0400

    vnet

--- azure-infra/environments/prod/variables.tf
+++ azure-infra/environments/prod/variables.tf
@@ -33,10 +33,6 @@ variable "address_space" {
   type        = list(string)
 }
 
-variable "location" {
-  description = "The Azure region where the resources will be created."
-  type        = string
-}
 
 variable "resource_group_name" {
   description = "The name of the resource group."
