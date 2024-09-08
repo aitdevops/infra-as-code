2d329f985ce1163b773c58c1da86f3cadb3a2ef9
rajeev3555-git <81769979+rajeev3555-git@users.noreply.github.com>
2024-09-08 15:36:05 -0400

    aks

--- azure-infra/modules/aks/main.tf
+++ azure-infra/modules/aks/main.tf
@@ -40,8 +40,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-# Output the AKS cluster kubeconfig
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config[0].config
-}
+# # Output the AKS cluster kubeconfig
+# output "kube_config" {
+#   value = azurerm_kubernetes_cluster.aks.kube_config[0].config
+# }
 
